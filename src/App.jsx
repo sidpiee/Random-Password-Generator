89162c9c6476db37448ad94d0d8037c5a02e7d29
Siddharth Sharma <sids23104@gmail.com>
2026-01-28 23:41:54 +0530

    add random password

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 export default function App() {
-  const [pass, setPass] = React.useState("");
   const [length, setLength] = React.useState(8);
   const [numAllowed, setNumAllowed] = React.useState(false);
   const [charAllowed, setCharAllowed] = React.useState(false);
+  const [pass, setPass] = React.useState("");
+
+  React.useEffect(() => {
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (numAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*";
+    let newpass = "";
+    for (let i = 1; i <= length; i++) {
+      newpass += str.charAt(Math.floor(Math.random() * str.length));
+    }
+    setPass(newpass);
+  }, [length, numAllowed, charAllowed]);
 
   return (
     <div className="flex items-center justify-center w-full h-screen bg-linear-to-br from-blue-950/80 to-violet-950">
@@ -12,7 +23,11 @@ export default function App() {
           Random Password generator
         </h1>
         <div className="flex m-5 overflow-hidden shadow rounded-2xl">
-          <input type="text" className="w-full px-3 py-1 bg-white " />
+          <input
+            type="text"
+            className="w-full px-3 py-1 bg-white "
+            value={pass}
+          />
           <button className="bg-blue-300 text-black px-3 py-0.5 shrink-0 cursor-pointer">
             copy
           </button>
@@ -23,9 +38,10 @@ export default function App() {
             min={8}
             max={20}
             name=""
-            id=""
+            id="length"
             value={length}
-            onChange={(e) => setLength(e.target.value)}
+            onChange={(e) => setLength(Number(e.target.value))}
+            readOnly
           />
           <label htmlFor="length">Length : {length}</label>
         </div>
